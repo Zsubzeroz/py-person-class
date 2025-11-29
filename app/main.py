d0b65f140d45a5758a8f822bf1edfe05f05bde02
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2025-11-29 14:32:03 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,27 +6,21 @@ class Person:
         self.age = age
         Person.people[name] = self
 
-    def __repr__(self):
-        return f"<Person {self.name} ({self.age})>"
 
-
-def create_person_list(people_data: list) -> list:
+def create_person_list(people: list) -> list:
     Person.people = {}
-    person_instances = []
+    person_list = []
 
-    for data in people_data:
-        p = Person(data['name'], data['age'])
-        person_instances.append(p)
+    for p in people:
+        person_list.append(Person(p['name'], p['age']))
 
-    for data in people_data:
-        current_person = Person.people[data['name']]
+    for p in people:
+        instance = Person.people[p['name']]
 
-        wife_name = data.get('wife')
-        if wife_name:
-            current_person.wife = Person.people[wife_name]
+        if p.get('wife'):
+            instance.wife = Person.people[p['wife']]
 
-        husband_name = data.get('husband')
-        if husband_name:
-            current_person.husband = Person.people[husband_name]
+        if p.get('husband'):
+            instance.husband = Person.people[p['husband']]
 
-    return person_instances
+    return person_list
